091caf1b10040d004a9e9781aac37a7d8f8bfff0
Orange-J98 <zcz19981202@163.com>
2020-10-24 19:41:36 +0800

    test

---
+++ src/main/java/miniplc0java/tokenizer/Tokenizer.java
@@ -0,0 +1,157 @@
+package miniplc0java.tokenizer;
+
+import miniplc0java.error.TokenizeError;
+import miniplc0java.error.ErrorCode;
+import miniplc0java.util.Pos;
+
+public class Tokenizer {
+
+    private StringIter it;
+    private String keyWord[] = { "begin", "end", "var", "const", "print"};
+    public Tokenizer(StringIter it) {
+        this.it = it;
+    }
+
+    // 这里本来是想实现 Iterator<Token> 的，但是 Iterator 不允许抛异常，于是就这样了
+    /**
+     * 获取下一个 Token
+     * 
+     * @return
+     * @throws TokenizeError 如果解析有异常则抛出
+     */
+    public Token nextToken() throws TokenizeError {
+        it.readAll();
+
+        // 跳过之前的所有空白字符
+        skipSpaceCharacters();
+
+        if (it.isEOF()) {
+            return new Token(TokenType.EOF, "", it.currentPos(), it.currentPos());
+        }
+
+        char peek = it.peekChar();
+        if (Character.isDigit(peek)) {
+            return lexUInt();
+        } else if (Character.isAlphabetic(peek)) {
+            return lexIdentOrKeyword();
+        } else {
+            return lexOperatorOrUnknown();
+        }
+    }
+
+    private Token lexUInt() throws TokenizeError {
+        // 请填空：
+        // 直到查看下一个字符不是数字为止:
+        // -- 前进一个字符，并存储这个字符
+        //
+        // 解析存储的字符串为无符号整数
+        // 解析成功则返回无符号整数类型的token，否则返回编译错误
+        //
+        // Token 的 Value 应填写数字的值
+        String token = "";
+        Pos startpos1 = it.currentPos();
+        while (Character.isDigit(it.peekChar())) {
+            token += it.nextChar();
+        }
+        Pos endpos1 = it.currentPos();
+        token = removeZero(token);
+        if (token.equals("")) {
+            token = "0";
+        }
+        return new Token(TokenType.Uint,token,startpos1,endpos1);
+    }
+
+    private Token lexIdentOrKeyword() throws TokenizeError {
+        // 请填空：
+        // 直到查看下一个字符不是数字或字母为止:
+        // -- 前进一个字符，并存储这个字符
+        //
+        // 尝试将存储的字符串解释为关键字
+        // -- 如果是关键字，则返回关键字类型的 token
+        // -- 否则，返回标识符
+        //
+        // Token 的 Value 应填写标识符或关键字的字符串
+        String token = "";
+        Pos startpos = it.currentPos();
+        while (Character.isAlphabetic(it.peekChar()) || Character.isDigit(it.peekChar())) {
+            token +=it.nextChar();
+        }
+        Pos endpos = it.currentPos();
+
+        int temp=isKey(token);
+        if (temp>0) {
+            switch (temp){
+                case 1:
+                    return new Token(TokenType.Begin,token,startpos,endpos);
+                case 2:
+                    return new Token(TokenType.End,token,startpos,endpos);
+                case 3:
+                    return new Token(TokenType.Var,token,startpos,endpos);
+                case 4:
+                    return new Token(TokenType.Const,token,startpos,endpos);
+                case 5:
+                    return new Token(TokenType.Print,token,startpos,endpos);
+                default:
+                    return new Token(TokenType.Ident,token,startpos,endpos);
+            }
+        } else{
+            return new Token(TokenType.Ident,token,startpos,endpos);
+        }
+    }
+
+    private Token lexOperatorOrUnknown() throws TokenizeError {
+        switch (it.nextChar()) {
+            case '+':
+                return new Token(TokenType.Plus, '+', it.previousPos(), it.currentPos());
+
+            case '-':
+                // 填入返回语句
+                return new Token(TokenType.Minus,'-',it.previousPos(),it.currentPos());
+
+            case '*':
+                // 填入返回语句
+                return  new Token(TokenType.Mult,'*',it.previousPos(),it.currentPos());
+
+            case '/':
+                // 填入返回语句
+                return  new Token(TokenType.Div,'/',it.previousPos(),it.currentPos());
+            // 填入更多状态和返回语句
+            case '=':
+                // 填入返回语句
+                return  new Token(TokenType.Equal,'=',it.previousPos(),it.currentPos());
+            case ';':
+                // 填入返回语句
+                return  new Token(TokenType.Semicolon,';',it.previousPos(),it.currentPos());
+            case '(':
+                // 填入返回语句
+                return  new Token(TokenType.LParen,'(',it.previousPos(),it.currentPos());
+            case ')':
+                // 填入返回语句
+                return  new Token(TokenType.RParen,')',it.previousPos(),it.currentPos());
+            default:
+                // 不认识这个输入，摸了
+                throw new TokenizeError(ErrorCode.InvalidInput, it.previousPos());
+        }
+    }
+
+    private void skipSpaceCharacters() {
+        while (!it.isEOF() && Character.isWhitespace(it.peekChar())) {
+            it.nextChar();
+        }
+    }
+    private String removeZero(String str) {
+        int len = str.length();
+        int i = 0;
+        while (i < len && str.charAt(i) == '0') {
+            i++;
+        }
+        return str.substring(i);
+    }
+    private int isKey(String str) {
+        for (int i = 0; i < keyWord.length; i++) {
+            if (keyWord[i].equals(str))
+                return (i+1);
+        }
+        return 0;
+    }
+}
