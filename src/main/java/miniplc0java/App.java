ddfaddd2aa61ff6186ecb06f20682c8d8f90aa04
Orange-J98 <zcz19981202@163.com>
2020-12-26 22:24:47 +0800

    Can't be Empty

---
+++ src/main/java/miniplc0java/App.java
@@ -0,0 +1,218 @@
+package miniplc0java;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
+import miniplc0java.error.CompileError;
+import miniplc0java.instruction.Instruction;
+import miniplc0java.tokenizer.StringIter;
+import miniplc0java.tokenizer.Token;
+import miniplc0java.tokenizer.TokenType;
+import miniplc0java.tokenizer.Tokenizer;
+import miniplc0java.analyser.*;
+
+import net.sourceforge.argparse4j.*;
+import net.sourceforge.argparse4j.impl.Arguments;
+import net.sourceforge.argparse4j.inf.ArgumentAction;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
+
+public class App {
+    public static void main(String[] args) throws CompileError {
+        var argparse = buildArgparse();
+        Namespace result;
+        try {
+            result = argparse.parseArgs(args);
+        } catch (ArgumentParserException e1) {
+            argparse.handleError(e1);
+            return;
+        }
+
+        var inputFileName = result.getString("input");
+        var outputFileName = result.getString("output");
+
+        InputStream input;
+        if (inputFileName.equals("-")) {
+            input = System.in;
+        } else {
+            try {
+                input = new FileInputStream(inputFileName);
+            } catch (FileNotFoundException e) {
+                System.err.println("Cannot find input file.");
+                e.printStackTrace();
+                System.exit(2);
+                return;
+            }
+        }
+
+        PrintStream output;
+        if (outputFileName.equals("-")) {
+            output = System.out;
+        } else {
+            try {
+                output = new PrintStream(new FileOutputStream(outputFileName));
+            } catch (FileNotFoundException e) {
+                System.err.println("Cannot open output file.");
+                e.printStackTrace();
+                System.exit(2);
+                return;
+            }
+        }
+
+        Scanner scanner;
+        scanner = new Scanner(input);
+        var iter = new StringIter(scanner);
+        var tokenizer = tokenize(iter);
+        var analyzer = new Analyser(tokenizer);
+        List<Instruction> instructions;
+        StringBuilder Binary = new StringBuilder();
+        HashMap<String, FuncEntry> funcTable= new HashMap<>();
+        if (result.getBoolean("tokenize")) {
+            // tokenize
+            var tokens = new ArrayList<Token>();
+            try {
+                while (true) {
+                    var token = tokenizer.nextToken();
+                    if (token.getTokenType().equals(TokenType.EOF)) {
+                        break;
+                    }
+                    tokens.add(token);
+                }
+            } catch (Exception e) {
+                // 遇到错误不输出，直接退出
+                System.err.println(e);
+                System.exit(0);
+                return;
+            }
+            for (Token token : tokens) {
+                output.println(token.toString());
+            }
+        } else if (result.getBoolean("analyse")) {
+            // analyze
+            var tokens = new ArrayList<Token>();
+            try {
+                funcTable = analyzer.analyse();
+                while (true) {
+                    var token = tokenizer.nextToken();
+                    if (token.getTokenType().equals(TokenType.EOF)) {
+                        break;
+                    }
+                    tokens.add(token);
+                }
+            } catch (Exception e) {
+                // 遇到错误不输出，直接退出
+                e.printStackTrace();
+                System.err.println(e);
+                System.exit(-1);
+                return;
+            }
+            //转换成二进制！
+            int [] maigicVersion ={0x72,0x30,0x3b,0x3e,0,0,0,0x01};
+            for (int num:maigicVersion){
+                Binary.append(toBinary_8(num));
+            }
+            Binary.append(toBinary_32(analyzer.getGlobalCounts()));
+            HashMap<String,SymbolEntry> globalTable= analyzer.getGlobalSymbolTable();
+            for(SymbolEntry tempGlobalEntry:globalTable.values()){
+                if (tempGlobalEntry.isConstant()){
+                    Binary.append(toBinary_8(1));
+                }else{
+                    Binary.append(toBinary_8(0));
+                }
+                Binary.append(toBinary_32(tempGlobalEntry.getGlobal_count()));
+                if (tempGlobalEntry.getGlobal_value().equals("")){
+                    Binary.append(toBinary_64(0));
+                }else{
+                    Binary.append(StrToBinary(tempGlobalEntry.getGlobal_value()));
+                }
+            }
+            Binary.append(toBinary_32(funcTable.size()));
+            for (FuncEntry funcEntry:funcTable.values()){
+                Binary.append(toBinary_32(funcEntry.getFunc_name()));
+                Binary.append(toBinary_32(funcEntry.getRet_num()));
+                Binary.append(toBinary_32(funcEntry.getParam_num()));
+                Binary.append(toBinary_32(funcEntry.getLocVarNum()));
+                Binary.append(toBinary_32(funcEntry.getBodyCnt()));
+                for (Instruction instruction:funcEntry.getInstructions()){
+                    if (instruction.getX()==-1||instruction.getX()==null) {
+                        Binary.append(toBinary_8(instruction.getOptNum()));
+                    }else {
+                        Binary.append(toBinary_8(instruction.getOptNum()));
+                        Binary.append(toBinary_64(instruction.getX()));
+                    }
+                }
+            }
+            output.print(Binary);
+        } else {
+            System.err.println("Please specify either '--analyse' or '--tokenize'.");
+            System.exit(3);
+        }
+    }
+    private  static String toBinary_8 (int num)
+    {
+        String binary_8=Integer.toBinaryString(num);
+        int bit = 8-binary_8.length();
+        if(binary_8.length()<8){
+            for(int j=0; j<bit; j++){
+                binary_8 = "0"+binary_8;
+            }
+        }
+        return binary_8;
+    }
+
+    private  static String toBinary_32 (int num)
+    {
+        String binary_32=Integer.toBinaryString(num);
+        int bit = 32-binary_32.length();
+        if(binary_32.length()<32){
+            for(int j=0; j<bit; j++){
+                binary_32 = "0"+binary_32;
+            }
+        }
+        return binary_32;
+    }
+
+    private  static String toBinary_64 (int num)
+    {
+        String binary_64=Integer.toBinaryString(num);
+        int bit = 64-binary_64.length();
+        if(binary_64.length()<64){
+            for(int j=0; j<bit; j++){
+                binary_64 = "0"+binary_64;
+            }
+        }
+        return binary_64;
+    }
+    private static String StrToBinary(String str) {
+        char[] strChar = str.toCharArray();
+        String result = "";
+        for (int i = 0; i < strChar.length; i++) {
+            result += Integer.toBinaryString(strChar[i]);
+        }
+        return result;
+    }
+    private static ArgumentParser buildArgparse() {
+        var builder = ArgumentParsers.newFor("miniplc0-java");
+        var parser = builder.build();
+        parser.addArgument("-t", "--tokenize").help("Tokenize the input").action(Arguments.storeTrue());
+        parser.addArgument("-l", "--analyse").help("Analyze the input").action(Arguments.storeTrue());
+        parser.addArgument("-o", "--output").help("Set the output file").required(true).dest("output")
+                .action(Arguments.store());
+        parser.addArgument("file").required(true).dest("input").action(Arguments.store()).help("Input file");
+        return parser;
+    }
+
+    private static Tokenizer tokenize(StringIter iter) {
+        var tokenizer = new Tokenizer(iter);
+        return tokenizer;
+    }
+}
---
+++ src/main/java/miniplc0java/tokenizer/Tokenizer.java
@@ -0,0 +1,257 @@
+package miniplc0java.tokenizer;
+import miniplc0java.error.*;
+import  miniplc0java.util.Pos;
+import java.util.regex.*;
+
+public class Tokenizer {
+    private StringIter it;
+    public Tokenizer(StringIter it){ this.it = it;}
+
+    /**
+     * 获取下一个 Token
+     *
+     * @return
+     * //@throws TokenizeError 如果解析有异常则抛出
+     */
+    public Token nextToken() throws TokenizeError {
+        it.readAll();
+
+        // 跳过之前的所有空白字符
+        skipSpaceCharacters();
+
+        if(it.isEOF()){
+            return new Token(TokenType.EOF,"",it.currentPos(),it.currentPos());
+        }
+
+        char peek = it.peekChar();
+        if(Character.isDigit(peek)){
+            return UIntOrDouble();
+        }else if(Character.isAlphabetic(peek)||peek=='_'){
+            return IdentOrKeyword();
+        }else if(peek=='"'||peek=='\''){
+            return StringOrChar();
+        }else if(peek=='/'){
+            return lexComent();
+        }else{
+            return OperatorOrUnknow();
+        }
+    }
+    //无符号整数或浮点数
+    private Token UIntOrDouble() throws TokenizeError{
+        String token = "";
+        Pos startpos1 = it.currentPos();
+        while (Character.isDigit(it.peekChar())) {
+            token += it.nextChar();
+        }
+        boolean flag = false;
+        if(it.peekChar()=='.'){
+            flag =true;
+            token += it.nextChar();
+            while (Character.isDigit(it.peekChar())) {
+                token += it.nextChar();
+            }
+            if(it.peekChar()=='e'||it.peekChar()=='E'){
+                token+=it.nextChar();
+                if(it.peekChar()=='+'||it.peekChar()=='-'){
+                    token+=it.nextChar();
+                }
+                while (Character.isDigit(it.peekChar())) {
+                    token += it.nextChar();
+                }
+            }
+        }
+
+        Pos endpos1 = it.currentPos();
+        token = removeZero(token);
+        if (token.equals("")) {
+            token = "0";
+        }
+
+        if(flag){
+            double num = Double.parseDouble(token);
+            return new Token(TokenType.DOUBLE_LITERAL,num,startpos1,endpos1);
+        }else {
+            int num = Integer.parseInt(token);
+            return new Token(TokenType.UINT_LITERAL,num, startpos1, endpos1);
+        }
+    }
+
+    //识别关键字和标识符
+    private Token IdentOrKeyword() throws TokenizeError{
+        String token = "";
+        Pos startpos = it.currentPos();
+        while(Character.isAlphabetic(it.peekChar())||Character.isDigit(it.peekChar())||it.peekChar()=='_'){
+            token += it.nextChar();
+        }
+        Pos endpos = it.currentPos();
+        switch (token){
+            case "fn":
+                return new Token(TokenType.FN_KW, token, startpos, endpos);
+            case "let":
+                return new Token(TokenType.LET_KW, token, startpos, endpos);
+            case "const":
+                return new Token(TokenType.CONST_KW, token, startpos, endpos);
+            case "as":
+                return new Token(TokenType.AS_KW, token, startpos, endpos);
+            case "while":
+                return new Token(TokenType.WHILE_KW, token, startpos, endpos);
+            case "if":
+                return new Token(TokenType.IF_KW, token, startpos, endpos);
+            case "else":
+                return new Token(TokenType.ELSE_KW, token, startpos, endpos);
+            case "return":
+                return new Token(TokenType.RETURN_KW, token, startpos, endpos);
+            case "break":
+                return new Token(TokenType.BREAK_KW, token, startpos, endpos);
+            case "continue":
+                return new Token(TokenType.CONTINUE_KW, token, startpos, endpos);
+            default:
+                return new Token(TokenType.IDENT,token,startpos,endpos);
+        }
+    }
+
+    private Token StringOrChar() throws TokenizeError{
+        String token = "";
+        Pos startpos1 =it.currentPos();
+        //字符串String
+        if (it.peekChar() == '\"'){
+            it.nextChar();
+            while(it.peekChar()!='\"'){
+                token = getEscapeSequence(token);
+            }
+            if (it.peekChar()=='\"'){
+                it.nextChar();
+                Pos endpos1 = it.currentPos();
+                return new Token(TokenType.STRING_LITERAL,token,startpos1,endpos1);
+            }else{
+                throw new TokenizeError(ErrorCode.InvalidInput,it.previousPos());
+            }
+        }else if(it.peekChar() == '\''){
+            //char字符串
+            it.nextChar();
+            while(it.peekChar()!='\''){
+                token = getEscapeSequence(token);
+            }
+            if (it.peekChar()=='\''){
+                it.nextChar();
+                Pos endpos1 = it.currentPos();
+                return new Token(TokenType.CHAR_LITERAL,token,startpos1,endpos1);
+            }else{
+                throw new TokenizeError(ErrorCode.InvalidInput,it.previousPos());
+            }
+        }else{
+            throw new TokenizeError(ErrorCode.InvalidInput,it.previousPos());
+        }
+    }
+    //识别转义字符
+    private String getEscapeSequence(String token) throws TokenizeError {
+        if(it.peekChar()=='\\'){
+            token+=it.nextChar();
+            if(it.peekChar()=='\\'||it.peekChar()=='\"'||it.peekChar()=='\''||it.peekChar()=='n'||it.peekChar()=='r'||it.peekChar()=='t'){
+                token+=it.nextChar();
+                return token;
+            }else{
+                throw new TokenizeError(ErrorCode.InvalidInput,it.previousPos());
+            }
+        }
+        token += it.nextChar();
+        return token;
+    }
+
+    //识别注释
+    private Token lexComent() throws TokenizeError{
+        String token = "";
+        Pos startpos1 = it.currentPos();
+        token+=it.nextChar();
+        if(it.peekChar()=='/'){
+            token+=it.nextChar();
+        }else{
+            throw new TokenizeError(ErrorCode.InvalidInput, it.previousPos());
+        }
+        while(it.peekChar()!='\n'){
+            token+=it.nextChar();
+        }
+        if(it.peekChar()=='\n'){
+            token+=it.nextChar();
+        }
+        Pos endpos1 = it.currentPos();
+        return new Token(TokenType.COMMENT,token,startpos1,endpos1);
+    }
+
+    //识别常量
+    private Token OperatorOrUnknow() throws TokenizeError{
+        switch (it.nextChar()){
+            case '+':
+                return new Token(TokenType.PLUS, '+', it.previousPos(), it.currentPos());
+            case '-':
+                if(it.peekChar()=='>'){
+                    it.nextChar();
+                    return new Token(TokenType.ARROW, "->", it.previousPos(), it.currentPos());
+                }else{
+                    return new Token(TokenType.MINUS, '-', it.previousPos(), it.currentPos());
+                }
+            case '*':
+                return new Token(TokenType.MUL, '*', it.previousPos(), it.currentPos());
+            case '/':
+                return new Token(TokenType.DIV, '/', it.previousPos(), it.currentPos());
+            case '=':
+                if (it.peekChar()=='='){
+                    it.nextChar();
+                    return new Token(TokenType.EQ,"==",it.previousPos(),it.currentPos());
+                }else{
+                    return new Token(TokenType.ASSIGN, '=', it.previousPos(), it.currentPos());
+                }
+            case '!':
+                if (it.peekChar()=='='){
+                    it.nextChar();
+                    return new Token(TokenType.NEQ,"!=",it.previousPos(),it.currentPos());
+                }else{
+                    throw new TokenizeError(ErrorCode.InvalidInput,it.previousPos());
+                }
+            case '<':
+                if (it.peekChar()=='='){
+                    it.nextChar();
+                    return new Token(TokenType.LE, "<=", it.previousPos(), it.currentPos());
+                }else{
+                    return new Token(TokenType.LT, '<', it.previousPos(), it.currentPos());
+                }
+            case '>':
+                if (it.peekChar()=='='){
+                    it.nextChar();
+                    return new Token(TokenType.GE,">=",it.previousPos(),it.currentPos());
+                }else{
+                    return new Token(TokenType.GT, '>', it.previousPos(), it.currentPos());
+                }
+            case '(':
+                return new Token(TokenType.L_PAREN, '(', it.previousPos(), it.currentPos());
+            case ')':
+                return new Token(TokenType.R_PAREN, ')', it.previousPos(), it.currentPos());
+            case '{':
+                return new Token(TokenType.L_BRACE, '{', it.previousPos(), it.currentPos());
+            case '}':
+                return new Token(TokenType.R_BRACE, '}', it.previousPos(), it.currentPos());
+            case ',':
+                return new Token(TokenType.COMMA, ',', it.previousPos(), it.currentPos());
+            case ':':
+                return new Token(TokenType.COLON, ':', it.previousPos(), it.currentPos());
+            case ';':
+                return new Token(TokenType.SEMICOLON, ';', it.previousPos(), it.currentPos());
+            default:
+                throw new TokenizeError(ErrorCode.InvalidInput, it.previousPos());
+        }
+    }
+
+    private void skipSpaceCharacters() {
+        while (!it.isEOF() && Character.isWhitespace(it.peekChar())) {
+            it.nextChar();
+        }
+    }
+    private String removeZero(String str) {
+        int len = str.length();
+        int i = 0;
+        while (i < len && str.charAt(i) == '0') {
+            i++;
+        }
+        return str.substring(i);
+    }
+}
