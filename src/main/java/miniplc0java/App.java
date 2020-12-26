44fbc22c693008f2fc7d5e31b4d06985dbbfb198
Orange-J98 <zcz19981202@163.com>
2020-12-27 01:48:30 +0800

    Byte Transition

--- src/main/java/miniplc0java/App.java
+++ src/main/java/miniplc0java/App.java
@@ -72,7 +72,8 @@ public class App {
         var tokenizer = tokenize(iter);
         var analyzer = new Analyser(tokenizer);
         List<Instruction> instructions;
-        StringBuilder Binary = new StringBuilder();
+        ArrayList<Byte> Binary = new ArrayList<>();
+
         HashMap<String, FuncEntry> funcTable= new HashMap<>();
         if (result.getBoolean("tokenize")) {
             // tokenize
@@ -113,98 +114,84 @@ public class App {
                 System.exit(-1);
                 return;
             }
-            //转换成二进制！
-            int [] maigicVersion ={0x72,0x30,0x3b,0x3e,0x0,0x0,0x0,0x01};
-            for (int num:maigicVersion){
-                Binary.append(toBinary_8(num));
-            }
-
-            Binary.append(toBinary_32(analyzer.getGlobalCounts()));
             HashMap<String,SymbolEntry> globalTable= analyzer.getGlobalSymbolTable();
             ArrayList<String> funcName = analyzer.getFuncName();
             ArrayList<String> globalName = analyzer.getGlobalName();
-
+            //转换成二进制！
+            int [] maigicVersion ={0x72,0x30,0x3b,0x3e,0x0,0x0,0x0,0x01};
+            for (int num:maigicVersion){
+                toBinary_8(Binary,num);
+            }
+            toBinary_32(Binary,analyzer.getGlobalCounts());
             for(String tempGlobalName:globalName){
                 SymbolEntry tempGlobalEntry = globalTable.get(tempGlobalName);
                 if (tempGlobalEntry.isConstant()){
-                    Binary.append(toBinary_8(1));
+                    toBinary_8(Binary,1);
                 }else{
-                    Binary.append(toBinary_8(0));
+                    toBinary_8(Binary,0);
                 }
-                Binary.append(toBinary_32(tempGlobalEntry.getGlobal_count()));
+                toBinary_32(Binary,tempGlobalEntry.getGlobal_count());
                 if (tempGlobalEntry.getGlobal_value().equals("")){
-                    Binary.append(toBinary_64(0));
+                    toBinary_64(Binary,0);
                 }else{
-                    Binary.append(StrToBinary(tempGlobalEntry.getGlobal_value()));
+                    StrToBinary(Binary,tempGlobalEntry.getGlobal_value());
                 }
             }
-
-            Binary.append(toBinary_32(funcTable.size()));
             for (String tempFuncName:funcName){
                 FuncEntry funcEntry = funcTable.get(tempFuncName);
-                Binary.append(toBinary_32(funcEntry.getFunc_name()));
-                Binary.append(toBinary_32(funcEntry.getRet_num()));
-                Binary.append(toBinary_32(funcEntry.getParam_num()));
-                Binary.append(toBinary_32(funcEntry.getLocVarNum()));
-                Binary.append(toBinary_32(funcEntry.getBodyCnt()));
+                toBinary_32(Binary,funcEntry.getFunc_name());
+                toBinary_32(Binary,funcEntry.getRet_num());
+                toBinary_32(Binary,funcEntry.getParam_num());
+                toBinary_32(Binary,funcEntry.getLocVarNum());
+                toBinary_32(Binary,funcEntry.getBodyCnt());
                 for (Instruction instruction:funcEntry.getInstructions()){
                     if (instruction.getX()==-1||instruction.getX()==null) {
-                        Binary.append(toBinary_8(instruction.getOptNum()));
+                        toBinary_8(Binary,instruction.getOptNum());
                     }else {
-                        Binary.append(toBinary_8(instruction.getOptNum()));
-                        Binary.append(toBinary_64(instruction.getX()));
+                        toBinary_8(Binary,instruction.getOptNum());
+                        toBinary_64(Binary,instruction.getX());
                     }
                 }
             }
-            output.print(Binary);
+            int length = Binary.size();
+            byte [] OutputByte = new byte[Binary.size()];
+            int i=0;
+            for (byte temp:Binary){
+                OutputByte[i++] = temp;
+            }
+            output.write(OutputByte);
+
         } else {
             System.err.println("Please specify either '--analyse' or '--tokenize'.");
             System.exit(-1);
         }
     }
-    private  static String toBinary_8 (int num)
+    private  static void toBinary_8 (ArrayList<Byte>Binary,int num)
     {
-        String binary_8=Integer.toBinaryString(num);
-        int bit = 8-binary_8.length();
-        if(binary_8.length()<8){
-            for(int j=0; j<bit; j++){
-                binary_8 = "0"+binary_8;
-            }
-        }
-        return binary_8;
+            Binary.add((byte) (num& 0xff));
     }
 
-    private  static String toBinary_32 (int num)
+    private  static void toBinary_32 (ArrayList<Byte>Binary,int num)
     {
-        String binary_32=Integer.toBinaryString(num);
-        int bit = 32-binary_32.length();
-        if(binary_32.length()<32){
-            for(int j=0; j<bit; j++){
-                binary_32 = "0"+binary_32;
-            }
+        for (int i=3;i>=0;i--) {
+            Binary.add((byte) (num >> (8 * i) & 0xff));
         }
-        return binary_32;
     }
 
-    private  static String toBinary_64 (int num)
+    private  static void toBinary_64 (ArrayList<Byte>Binary,int num)
     {
-        String binary_64=Integer.toBinaryString(num);
-        int bit = 64-binary_64.length();
-        if(binary_64.length()<64){
-            for(int j=0; j<bit; j++){
-                binary_64 = "0"+binary_64;
-            }
+        for (int i=7;i>=0;i--) {
+            Binary.add((byte) (num >> (8 * i) & 0xff));
         }
-        return binary_64;
     }
-    private static String StrToBinary(String str) {
-        char[] strChar = str.toCharArray();
-        String result = "";
-        for (int i = 0; i < strChar.length; i++) {
-            result += Integer.toBinaryString(strChar[i]);
+
+    private static void StrToBinary(ArrayList<Byte>Binary,String str) {
+        byte[] s = str.getBytes();
+        for (byte tempS : s) {
+            Binary.add(tempS);
         }
-        return result;
     }
+
     private static ArgumentParser buildArgparse() {
         var builder = ArgumentParsers.newFor("miniplc0-java");
         var parser = builder.build();
